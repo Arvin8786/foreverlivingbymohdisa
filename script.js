868375e759c0ef0f5bab0b2fedf0974fd71e90cb
Forever Living <arvin8786@gmail.com>
2025-10-12 20:55:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // ===========================================================
 // [ 1.0 ] GLOBAL CONFIGURATION & STATE
 // ===========================================================
-const googleScriptURL = 'https://script.google.com/macros/s/AKfycbwZ9n0QWWLKqJZZe5_n6WB1VIvWossuo4yZxmt8fXsMQm1sWoR6EkkfMuyBexwHA06XOA/exec';
+const googleScriptURL = 'https://script.google.com/macros/s/AKfycbxThcqZXw4GhGHCck2iAQdpTPLC2drF-zM2RAMLQt5gqHN6AugBXoiEIuElkUDnsoeyiQ/exec';
 const botServerURL = 'https://whatsapp-eshop-bot.onrender.com/eshop-chat';
 const apiKey = '9582967';
 
